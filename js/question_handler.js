7f55129671422f1dd6546f69538a41794f7b57bd
Can Sagturk <C.Sagturk@student.tudelft.nl>
2021-07-21 12:20:43 +0300

    Added the stub how question handler script

---
+++ js/question_handler.js
@@ -0,0 +1,11 @@
+// TESTING DATA
+var testQuestion = {
+  text: 'Test question Test question Test question Test question Test question Test question Test question Test question'
+}
+//
+
+function replaceQuestion(question) {
+  document.getElementById('question-font').innerHTML = question.text;
+}
+
+// document.addEventListener('DOMContentLoaded', replaceQuestion(testQuestion), false);
