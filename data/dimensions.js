637a0578425d8fdf084871154c82761096cbf63f
Can Sagturk <C.Sagturk@student.tudelft.nl>
2021-07-23 11:31:41 +0300

    Added dimensions page functionality

---
+++ data/dimensions.js
@@ -0,0 +1,32 @@
+var dimensions = [
+  {
+    "name": "Nationalism",
+    "quote": "“Our country ought to be dearer to us than ourselves.” - Cicero",
+    "text": "Higher Nationalism scores imply an intimacy with one’s nation, its people, values, history, and culture. Those who agree with Nationalism are tend to prefer the wellbeing of their nation and people over others’.",
+    "imageSrc": "../images/nationalism.png"
+  },
+  {
+    "name": "Internationalism",
+    "quote": "“The love of one’s country is a splendid thing, but why should love stop at the border?” - Pablo Casals",
+    "text": "Higher Internationalism scores indicate a preference for an international community where nationalities and borders are limited to classifications. Those who agree with Internationalism prefer connectivity and cooperation between nations.",
+    "imageSrc": "../images/internationalism.png"
+  },
+  {
+    "name": "Globalism",
+    "quote": "“The world is now too small for anything but brotherhood.” - Arthur Powell Davies",
+    "text": "Higher Globalism scores signal an agreement with the idea of a singularly defined world rather than isolated communities interacting with one another. Those who agree with Globalism advocate for global economic and foreign policies rather than relationship-based policies.",
+    "imageSrc": "../images/globalism.png"
+  },
+  {
+    "name": "Isolationism",
+    "quote": "“Peace, commerce and honest friendship with all nations; entangling alliances with none.” - Thomas Jefferson",
+    "text": "Higher Isolationism scores suggest a preference for self-sustainable and closed economy. Those who agree with Isolationism support a nationally contained economy with self-oriented foreign policies, where intervention in other countries is abandoned.",
+    "imageSrc": "../images/isolationism.png"
+  },
+  {
+    "name": "",
+    "quote": "",
+    "text": "",
+    "imageSrc": "../images/internationalism.png"
+  }
+]
---
+++ js/dimensions_handler.js
@@ -0,0 +1,29 @@
+const buttonColor = '#0e446c';
+const selectedColor = '#11334c';
+const buttonIdPrefix = 'dimensions_button_';
+
+var selectedButton = 0;
+
+function dimensionsPageInit() {
+  var index = 0;
+  dimensions.forEach(element => {
+    document.getElementById('dimensions_panel').innerHTML +=
+      '<button class="d_button" id="dimensions_button_' + index + '" onclick="dimensionOnClick(' + index + ')">' + element.name + '</button>';
+    index++;
+  });
+  dimensionOnClick(selectedButton);
+}
+
+function dimensionOnClick(index) {
+  document.getElementById(buttonIdPrefix + selectedButton).style.backgroundColor = buttonColor;
+  loadContent(index);
+  document.getElementById(buttonIdPrefix + index).style.backgroundColor = selectedColor;
+  selectedButton = index;
+}
+
+function loadContent(index) {
+  var current = dimensions[index];
+  document.getElementById('dimension_image').style.backgroundImage = 'url("' + current.imageSrc +'")';
+  document.getElementById('quote_text').innerHTML = current.quote;
+  document.getElementById('description_text').innerHTML = current.text;
+}
