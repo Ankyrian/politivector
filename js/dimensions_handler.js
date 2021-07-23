b7aadefe45361fcda228ef0ce006ec58eaf98b3a
Can Sagturk <C.Sagturk@student.tudelft.nl>
2021-07-23 15:34:18 +0300

    Dimensions page mobile page

--- js/dimensions_handler.js
+++ js/dimensions_handler.js
@@ -2,15 +2,24 @@ const buttonColor = '#0e446c';
 const selectedColor = '#11334c';
 const buttonIdPrefix = 'dimensions_button_';
 
+var droppedDown = false;
+var matchWindowSize = window.matchMedia("(max-width: 800px)");
 var selectedButton = 0;
+var dimensionButtonHTML;
 
 function dimensionsPageInit() {
+  var dropdownButton = document.getElementById('dropdown_button');
+  dropdownButton.style.visibility = matchWindowSize.matches ? "visible" : "hidden";
+  dropdownButton.style.position = matchWindowSize.matches ? "static" : "absolute";
+  matchWindowSize.addListener(dropdownAdder);
   var index = 0;
+  var dimensionButtons = document.getElementById('dimension_buttons');
   dimensions.forEach(element => {
-    document.getElementById('dimensions_panel').innerHTML +=
+    dimensionButtons.innerHTML +=
       '<button class="d_button" id="' + buttonIdPrefix + index + '" onclick="dimensionOnClick(' + index + ')">' + element.name + '</button>';
     index++;
   });
+  dimensionButtonHTML = dimensionButtons.innerHTML;
   dimensionOnClick(selectedButton);
 }
 
@@ -22,4 +31,22 @@ function dimensionOnClick(index) {
   document.getElementById('description_text').innerHTML = current.text;
   document.getElementById(buttonIdPrefix + index).style.backgroundColor = selectedColor;
   selectedButton = index;
+  if(matchWindowSize.matches) {
+    dropdownOnClick();
+  }
+}
+
+function dropdownAdder() {
+  location.reload();
+}
+
+function dropdownOnClick() {
+  if(droppedDown) {
+    document.getElementById('dimension_buttons').innerHTML = "";
+    droppedDown = false;
+  }
+  else {
+    document.getElementById('dimension_buttons').innerHTML = dimensionButtonHTML;
+    droppedDown = true;
+  }
 }
