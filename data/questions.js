cbf646f05b529843b1bdde56cdf3fcfca7b29f90
Can Sagturk <C.Sagturk@student.tudelft.nl>
2021-08-04 16:55:38 +0300

    Added the questions enumaration and some trial data to test JS question notation

---
+++ data/questions.js
@@ -0,0 +1,74 @@
+/*
+  "ANTHROPOCENTRISM": 0,
+  "ASSIMILATIONISM": 1,
+  "AUTHORITARIANISM": 2,
+  "AUTOCRACY": 3,
+  "CAPITALISM": 4,
+  "COMMUNISM": 5,
+  "CONSERVATISM": 6,
+  "DEMOCRACY": 7,
+  "ECOCENTRISM": 8,
+  "ELITISM": 9,
+  "GLOBALISM": 10,
+  "INTERNATIONALISM": 11,
+  "ISOLATIONISM": 12,
+  "LIBERTARIANISM": 13,
+  "MERITOCRACY": 14,
+  "MILITARISM": 15,
+  "MODERNISM": 16,
+  "NATIONALISM": 17,
+  "PACIFISM": 18,
+  "PARTISANSHIP": 19,
+  "PLURALISM": 20,
+  "POPULISM": 21,
+  "SECULARISM": 22,
+  "THEOCRACY": 23
+*/
+
+var questions = [
+  {
+    "text": "Exploitation of the environment for human benefit is legitimate.",
+    "dimension": 0,
+    "aa": [7],
+    "pa": [5, 0.5],
+    "n": [0, 3],
+    "pd": [2, 0.5],
+    "ad": [0]
+  },
+  {
+    "text": "Humans, either through evolutionary superiority or divine decree, are the rightful owners of nature.",
+    "dimension": 0,
+    "aa": [7],
+    "pa": [5, 0.5],
+    "n": [0, 3],
+    "pd": [2, 0.5],
+    "ad": [0]
+  },
+  {
+    "text": "The value of a single human life is greater than the value of a small ecosystem.",
+    "dimension": 0,
+    "aa": [7],
+    "pa": [5, 0.5],
+    "n": [0, 3],
+    "pd": [2, 0.5],
+    "ad": [0]
+  },
+  {
+    "text": "Civilizational advancement cannot be abandoned for the sake of inconsequential effects on nature.",
+    "dimension": 0,
+    "aa": [7],
+    "pa": [5, 0.5],
+    "n": [0, 3],
+    "pd": [2, 0.5],
+    "ad": [0]
+  },
+  {
+    "text": "Without sentience to observe it, nature has no value.",
+    "dimension": 0,
+    "aa": [7],
+    "pa": [5, 0.5],
+    "n": [0, 3],
+    "pd": [2, 0.5],
+    "ad": [0]
+  }
+]
--- js/question_handler.js
+++ js/question_handler.js
@@ -1,3 +1,32 @@
+const Dimensions = Object.freeze(
+  {
+    "ANTHROPOCENTRISM": 0,
+    "ASSIMILATIONISM": 1,
+    "AUTHORITARIANISM": 2,
+    "AUTOCRACY": 3,
+    "CAPITALISM": 4,
+    "COMMUNISM": 5,
+    "CONSERVATISM": 6,
+    "DEMOCRACY": 7,
+    "ECOCENTRISM": 8,
+    "ELITISM": 9,
+    "GLOBALISM": 10,
+    "INTERNATIONALISM": 11,
+    "ISOLATIONISM": 12,
+    "LIBERTARIANISM": 13,
+    "MERITOCRACY": 14,
+    "MILITARISM": 15,
+    "MODERNISM": 16,
+    "NATIONALISM": 17,
+    "PACIFISM": 18,
+    "PARTISANSHIP": 19,
+    "PLURALISM": 20,
+    "POPULISM": 21,
+    "SECULARISM": 22,
+    "THEOCRACY": 23
+  }
+)
+
 // TESTING DATA
 var testQuestion = {
   text: 'Test question Test question Test question Test question Test question Test question Test question Test question'
