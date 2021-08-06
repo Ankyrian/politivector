3b45f87f849d9373cd23055cd84e0cffffebb7d8
Can Sagturk <C.Sagturk@student.tudelft.nl>
2021-08-07 01:10:21 +0300

    Made the class and id names in files related to dimension view consistent

--- js/dimensions_handler.js
+++ js/dimensions_handler.js
@@ -1,6 +1,6 @@
 const buttonColor = '#0e446c';
 const selectedColor = '#11334c';
-const buttonIdPrefix = 'dimensions_button_';
+const buttonIdPrefix = 'dimensions-button-';
 
 var droppedDown = false;
 var matchWindowSize = window.matchMedia("(max-width: 800px)");
@@ -8,15 +8,15 @@ var selectedButton = 0;
 var dimensionButtonHTML;
 
 function dimensionsPageInit() {
-  var dropdownButton = document.getElementById('dropdown_button');
+  var dropdownButton = document.getElementById('dropdown-button');
   dropdownButton.style.visibility = matchWindowSize.matches ? "visible" : "hidden";
   dropdownButton.style.position = matchWindowSize.matches ? "static" : "absolute";
   matchWindowSize.addListener(dropdownAdder);
   var index = 0;
-  var dimensionButtons = document.getElementById('dimension_buttons');
+  var dimensionButtons = document.getElementById('dimension-buttons');
   dimensions.forEach(element => {
     dimensionButtons.innerHTML +=
-      '<button class="d_button" id="' + buttonIdPrefix + index + '" onclick="dimensionOnClick(' + index + ')">' + element.name + '</button>';
+      '<button class="d-button" id="' + buttonIdPrefix + index + '" onclick="dimensionOnClick(' + index + ')">' + element.name + '</button>';
     index++;
   });
   dimensionButtonHTML = dimensionButtons.innerHTML;
@@ -26,9 +26,9 @@ function dimensionsPageInit() {
 function dimensionOnClick(index) {
   document.getElementById(buttonIdPrefix + selectedButton).style.backgroundColor = buttonColor;
   var current = dimensions[index];
-  document.getElementById('dimension_image').style.backgroundImage = 'url("' + current.imageSrc +'")';
-  document.getElementById('quote_text').innerHTML = current.quote;
-  document.getElementById('description_text').innerHTML = current.text;
+  document.getElementById('dimension-image').style.backgroundImage = 'url("' + current.imageSrc +'")';
+  document.getElementById('quote-text').innerHTML = current.quote;
+  document.getElementById('description-text').innerHTML = current.text;
   document.getElementById(buttonIdPrefix + index).style.backgroundColor = selectedColor;
   selectedButton = index;
   if(matchWindowSize.matches) {
@@ -42,11 +42,11 @@ function dropdownAdder() {
 
 function dropdownOnClick() {
   if(droppedDown) {
-    document.getElementById('dimension_buttons').innerHTML = "";
+    document.getElementById('dimension-buttons').innerHTML = "";
     droppedDown = false;
   }
   else {
-    document.getElementById('dimension_buttons').innerHTML = dimensionButtonHTML;
+    document.getElementById('dimension-buttons').innerHTML = dimensionButtonHTML;
     droppedDown = true;
   }
 }
