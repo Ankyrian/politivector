143e9d779a4d8d49710e4de402176c0bafbb59e5
Can Sagturk <C.Sagturk@student.tudelft.nl>
2022-08-22 19:15:07 +0300

    Added the results model

---
+++ models/ResultModel.js
@@ -0,0 +1,8 @@
+let mongoose = require('mongoose')
+
+let resultSchema = new mongoose.Schema({
+  ip: String,
+  resultsArrays: [ {type: Map, of: Number} ]
+})
+
+module.exports = mongoose.model('Result', resultSchema)
